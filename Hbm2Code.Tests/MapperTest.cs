6716d275a79fe10b9d944cbb50110a679e2aabea
HungHQ <profhahung@gmail.com>
2019-09-03 23:13:07 +0700

    Add MapperTest

---
+++ Hbm2Code.Tests/MapperTest.cs
@@ -0,0 +1,47 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace Hbm2Code.Tests
+{
+    public class MapperTest
+    {
+        [Fact]
+        public void MapAttribute_StringValueShouldHaveDoubleQuotes()
+        {
+            var prop = new Property(null, "test")
+            {
+                { "index", "IX_TEST" }
+            };
+
+            Mapper.MapAttributeValue(prop, "index").Should().Be(@"""IX_TEST""");
+        }
+
+        [Fact]
+        public void MapAttribute_NameHavingDashShouldBecomePascalCase()
+        {
+            Mapper.MapAttributeMethod(null, "foreign-key").Should().Be("ForeignKey");
+        }
+
+        [Fact]
+        public void MapAttribute_IndexOfMap_ShouldBecomeElement()
+        {
+            var indexProp = new Property(null, "")
+            {
+                TagName = "index"
+            };
+
+            Mapper.MapAttributeMethod(indexProp, "index").Should().Be("Element");
+        }
+
+        [Fact]
+        public void MapAttribute_ManyToManyOfMap_ShouldBecomeManyToMany()
+        {
+            var indexProp = new Property(null, "")
+            {
+                TagName = "map-key-many-to-many"
+            };
+
+            Mapper.MapAttributeMethod(indexProp, "map-key-many-to-many").Should().Be("ManyToMany");
+        }
+    }
+}

2bb92c0ebeb5b2849ad4f22d86a94fbc0e00651e
HungHQ <profhahung@gmail.com>
2019-09-07 14:49:38 +0700

    Allow search pattern when loading hbm files

--- Hbm2Code/HbmLoader.cs
+++ Hbm2Code/HbmLoader.cs
@@ -7,12 +7,12 @@ namespace Hbm2Code
 {
     public static class HbmLoader
     {
-        public static IList<ClassInfo> LoadClassInfos(string hbmFolderPath)
+        public static IList<ClassInfo> LoadClassInfos(string hbmFolderPath, string searchPattern = "*.hbm.xml")
         {
             var result = new List<ClassInfo>();
 
             Console.WriteLine("Reading hbm files from " + hbmFolderPath);
-            foreach (string file in Directory.GetFiles(hbmFolderPath, "*.hbm.xml", SearchOption.AllDirectories))
+            foreach (string file in Directory.GetFiles(hbmFolderPath, searchPattern, SearchOption.AllDirectories))
             {
                 var parser = new HbmParser(XDocument.Load(file));
                 result.AddRange(parser.Parse());
